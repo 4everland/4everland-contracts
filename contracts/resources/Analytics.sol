b0102d176b072100edc469ed96d0c6da5ede4aa1
Alexandas <Alexandas@4everland.com>
2023-04-19 15:43:32 +0800

    upgrade analytics

--- contracts/resources/Analytics.sol
+++ contracts/resources/Analytics.sol
@@ -18,6 +18,19 @@ contract Analytics is RouterWrapper {
 
 	function send(uint256 nonce, ResourceData.AmountPayload[] memory payload) external onlyProvider {
 		require(!nonces[nonce], 'Analytics: nonce exists');
+		require(payload.length > 0, 'Analytics: invalid payload length');
+		for (uint256 i = 0; i < payload.length; i++) {
+			ResourceData.AmountPayload memory p = payload[i];
+			if (
+				p.resourceType == ResourceData.ResourceType.BuildingTime ||
+				p.resourceType == ResourceData.ResourceType.Bandwidth ||
+				p.resourceType == ResourceData.ResourceType.ARStorage
+			) {
+				require(p.amounts.length == 1, 'Analytics: invalid payload');
+			} else if (p.resourceType == ResourceData.ResourceType.IPFSStorage) {
+				require(p.amounts.length == 2, 'Analytics: invalid payload');
+			}
+		}
 		nonces[nonce] = true;
 		emit Drip(msg.sender, nonce, payload);
 	}
